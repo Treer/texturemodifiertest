337a71467e96e828f0ca3b23cc999744e352b220
Treer <treer.git+github@the-bordello.com>
2021-09-26 05:08:21 +1000

    Test ^[fill with optional offsets

--- fill.lua
+++ fill.lua
@@ -8,6 +8,12 @@ minetest.register_node("texturemodifiertest:fill_red", {
 })
 
 
+minetest.register_node("texturemodifiertest:fill_redgreen_samesize", {
+	description = "16x16 47%-transparent red blended on top of a 16x16 solid green",
+	tiles = {"[fill:16x16:green^[fill:16x16:#F007"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
 minetest.register_node("texturemodifiertest:fill_redgreen", {
 	description = "8x8 47%-transparent red blended on top of a 16x16 solid green",
 	tiles = {"[fill:16x16:green^[fill:8x8:#F007"},
@@ -45,7 +51,7 @@ minetest.register_node("texturemodifiertest:fill_greenbluetransSize", {
 
 minetest.register_node("texturemodifiertest:fill_combine", {
 	description = "[combine colored rects on a solid grey background",
-	tiles = {"[fill:33x33:#222^[combine:64x64:16,16=[fill\\:16x16\\:green:32,16=[fill\\:16x32\\:#00F8"},
+	tiles = {"[fill:64x64:#222^[combine:64x64:16,16=[fill\\:16x16\\:green:32,16=[fill\\:16x32\\:#00F8"},
 	groups = {cracky = 1, oddly_breakable_by_hand = 1},
 	use_texture_alpha="blend"
 })
@@ -66,3 +72,76 @@ minetest.register_alias("texturemodifiertest:rect_greenbluetrans",     "texturem
 minetest.register_alias("texturemodifiertest:rect_greenbluetransSize", "texturemodifiertest:fill_greenbluetransSize")
 minetest.register_alias("texturemodifiertest:rect_combine",            "texturemodifiertest:fill_combine")
 minetest.register_alias("texturemodifiertest:rect_combine2",           "texturemodifiertest:fill_combine2")
+
+
+
+--== test [fill with offset x,y ==--
+
+minetest.register_node("texturemodifiertest:fill_red_center", {
+	description = "8x8 red square centered on a 16x16 green background",
+	tiles = {"[fill:16x16:green^[fill:8x8:4,4:#F00"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_red_clipped_bottomright", {
+	description = "8x8 red square falling off the bottom right corner of a 16x16 green background, only 4x4 red is showing",
+	tiles = {"[fill:16x16:green^[fill:8x8:12,12:#F00"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_red_clipped_topleft", {
+	description = "8x8 red square falling off the top left corner of a 16x16 green background, only 4x4 red is showing",
+	tiles = {"[fill:16x16:green^[fill:8x8:-4,-4:#F00"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_red_clipped_topright", {
+	description = "8x8 red square falling off the top right corner of a 16x16 green background, only 4x4 red is showing",
+	tiles = {"[fill:16x16:green^[fill:8x8:12,-4:#F00"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_red_clipped_bottomleft", {
+	description = "8x8 red square falling off the bottom left corner of a 16x16 green background, only 4x4 red is showing",
+	tiles = {"[fill:16x16:green^[fill:8x8:-4,12:#F00"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_lines", {
+	description = "14x14 colourful frame drawn on a 16x16 green background",
+	tiles = {"[fill:16x16:green^[fill:14x1:1,1:#F00^[fill:14x1:1,14:#F80^[fill:1x14:1,1:#00F^[fill:1x14:14,1:#08F"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+minetest.register_node("texturemodifiertest:fill_redgreen_samesize_zerooffset", {
+	description = "16x16 47%-transparent red blended on top of a 16x16 solid green, specifying an offset of 0,0 - which should look no different to texturemodifiertest:fill_redgreen_samesize",
+	tiles = {"[fill:16x16:green^[fill:16x16:0,0:#F007"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+minetest.register_node("texturemodifiertest:fill_redgreen_samesize_baseoffsetignored", {
+	description = "16x16 47%-transparent red blended on top of a 16x16 solid green, where the green specified an offset that should ignored, so should look no different to texturemodifiertest:fill_redgreen_samesize",
+	tiles = {"[fill:16x16:-30,-30:green^[fill:16x16:#F007"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+minetest.register_node("texturemodifiertest:fill_yellow_center", {
+	description = "checks that [fill doesn't change the size of the base image, should be a 8x8 yellow square centered on a 16x16 green background",
+	tiles = {"[fill:16x16:red^[fill:32x32:green^[fill:8x8:4,4:yellow"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
+
+
+minetest.register_node("texturemodifiertest:fill_malformed1", {
+	description = "test handling of malformed string [fill:16x16:-30:green",
+	tiles = {"[fill:16x16:-30:green"},
+	groups = {cracky = 1, oddly_breakable_by_hand = 1},
+	use_texture_alpha="blend"
+})
